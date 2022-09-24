be678578bb8f53c6f876d4d834a939a1e3248efd
yusuf <af003yusuf@gmail.com>
2022-09-24 10:19:36 +0700

    .

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
-//ini golang
-//import ini wkwkwk
+// ini golang
+// import ini wkwkwk
 package main //nama package tu nama programnya?
 import "fmt" //buat print
 //mesti dalam function main baru jalan (mirip void loop di arduino)
@@ -14,22 +14,25 @@ func main() {
 	//setiap variabel yg dideklarasiin harus dipake, kalo gk error (forced to write a clean code,damn golang!)
 	//deklarasi variabel sekaligus
 	var (
-		angka = 1
-	    //iniString = "ini string"
-		//iniBoolean =true
-		//iniStringJuga string
-		//iniStringJuga = "dudul"
-		//iniArray = [...]int{1,2,3,4}
+		angka         = 1
+		iniString     = "ini string"
+		iniBoolean    = true
+		iniStringJuga string
+		iniArray      = [...]int{1, 2, 3, 4}
 	)
-	fmt.Print(angka)
-	//loop()
+	fmt.Println(angka)
+	fmt.Println(iniString)
+	fmt.Println(iniBoolean)
+	fmt.Println(iniStringJuga)
+	fmt.Println(iniArray)
+	loop()
 }
 
 func loop() {
 	sum := 0
 	//range 1 sampai kurang dari 5, mirip loop di cpp tapi ga pake kurung
 	for i := 1; i < 5; i++ {
-    sum += i
-}
-	fmt.Println(sum) // 10 (1+2+3+4) 
+		sum += i
+	}
+	fmt.Println(sum) // 10 (1+2+3+4)
 }
